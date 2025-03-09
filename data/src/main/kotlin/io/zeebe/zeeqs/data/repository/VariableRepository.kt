844b7bcfe997de9162fa0fb04bb90d3efd0e11fb
Hussain Mubaireek <hussain.mubaireek@cntxt.com>
2025-03-09 16:29:09 +0300

    fix: slow response to variables query

--- data/src/main/kotlin/io/zeebe/zeeqs/data/repository/VariableRepository.kt
+++ data/src/main/kotlin/io/zeebe/zeeqs/data/repository/VariableRepository.kt
@@ -1,14 +1,17 @@
 package io.zeebe.zeeqs.data.repository
 
 import io.zeebe.zeeqs.data.entity.Variable
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
+import javax.persistence.QueryHint
 
 @Repository
 interface VariableRepository : PagingAndSortingRepository<Variable, Long> {
 
     @Transactional(readOnly = true)
+    @QueryHints(value = [QueryHint(name = "org.hibernate.fetchSize", value = "1000")])
     fun findByProcessInstanceKey(processInstanceKey: Long): List<Variable>
 
     @Transactional(readOnly = true)
